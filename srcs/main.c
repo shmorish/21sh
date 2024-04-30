2186b242814f72d605e44f46ece3ce418b773b8d
shmorish <110565242+shmorish@users.noreply.github.com>
2024-04-30 16:47:26 +0900

    add exit command line

--- srcs/main.c
+++ srcs/main.c
@@ -1,24 +1,45 @@
-#include <readline.h>
-#include <history.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <readline.h>
+#include <history.h>
+
+pid_t get_own_pid(void)
+{
+	pid_t pid;
+	pid_t ownpid;
+
+	pid = fork();
+	ownpid = pid - 1;
+	if (pid == 0)
+		exit(0);
+	waitpid(pid, NULL, 0);
+	return ownpid;
+}
+
+void	exit_command_line(int exit_status)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDERR_FILENO, "exit\n", 6);
+	exit(exit_status);
+}
 
 int main() {
 	char *line;
 
 	rl_instream = stdin;
 	rl_outstream = stderr;
-	pid_t pid = fork();
-	if (pid == 0)
-		exit(0);
-	waitpid(pid, NULL, 0);
-	printf("pid: %d\n", pid);
+	pid_t pid = get_own_pid();
+	printf("getpid: ours %d\n", pid);
+	printf("getpid: func %d\n", getpid());
+	int	exit_status = 0;
+
 	while (1)
 	{
 		line = readline("> ");
 		if (!line)
-			break ;
+			exit_command_line(exit_status);
 		add_history(line);
 		free(line);
 	}
