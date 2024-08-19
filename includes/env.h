a9a921cb320c5ce0a6b38e80232ef5b04ca45bdc
shmorish <110565242+shmorish@users.noreply.github.com>
2024-08-20 02:56:21 +0900

    changed default path to define

--- includes/env.h
+++ includes/env.h
@@ -16,8 +16,13 @@
 # include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/wait.h>
 # include "libft.h"
 # include "utils.h"
+
+# define DEFAULT_PATH "/usr/local/bin:/bin:/usr/bin:."
 
 typedef struct s_env	t_env;
 
@@ -48,6 +53,9 @@ char	*split_env_to_key(char *envp);
 char	*split_env_to_value(char *envp);
 void	free_all_env(void);
 
+// ft_getpid.c
+pid_t	ft_getpid(void);
+
 // get_env.c
 char	*get_env_value(char *key);
 t_env	*get_env_by_key(char *key);
@@ -59,5 +67,8 @@ t_env	*get_env_head(void);
 // print_env.c
 void	print_env(void);
 void	print_env_export(void);
+
+// shlvl_addition.c
+void	shlvl_addition(void);
 
 #endif
